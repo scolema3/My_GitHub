fbc2f300d3239522f898335976c3b44974c3ef18
Shawn Coleman <scolema3@gmail.com>
2013-09-12 15:56:43 -0500

    Changed fix_ave_time_saed to allow multiple snapshots -- address issue with box size changes

--- lammps-27Aug13/src/fix_ave_time_saed.h
+++ lammps-27Aug13/src/fix_ave_time_saed.h
@@ -38,6 +38,7 @@ class FixAveTimeSAED : public Fix {
   double compute_array(int,int);
   void reset_timestep(bigint);
 
+
  private:
   int me,nvalues;
   int nrepeat,nfreq,irepeat;
@@ -81,8 +82,10 @@ class FixAveTimeSAED : public Fix {
   double  c[3];              // Parameters controlling resolution of reciprocal space explored
   double  dR_Ewald;          // Thickness of Ewald sphere slice
   double  prd_inv[3];        // Inverse spacing of unit cell
- 
 
+  char    *filename;         // user-specified file
+  int     nOutput; 
+  int     Dim[3];
 
 
 
